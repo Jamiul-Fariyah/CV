21ed86f99db4dad0fe72c637f3da95cc8155c47d
jim <jamiulfariyahjim17@gmail.com>
2021-03-21 00:19:50 +0600

    mid

---
+++ family_details.php
@@ -0,0 +1,37 @@
+<htlm>
+	<head>
+		<title>Family Details| Jamiul Fariyah</title>
+	</head
+	<body>
+		<p><br></p>
+		
+		
+		<h1>Family Details</h1>
+		
+		
+		<ul style="padding-left: 140px" type="disk" >
+			<li><b>Father's Name: </b>Late Jasim Uddin Ashkari </li>
+			<p><br></p>
+			<li><b>Mother's Name: </b>Hasina Chowdhury</li>
+			<li><b>Mother's Occupation: </b>Service Holder</li>
+		</ul>
+		
+		<p><br></p>
+
+	    <a href="my_cv.html">Profile</a>
+	
+	    <a href="education.html" >Education</a>
+
+	    <a href="family_details.html" >Family Details</a>
+
+	    <a href="achievements.html" >Achievements</a>
+
+	    <a href="experience.html" >Experience</a>
+
+	    <a href="reference.html" >Reference</a>
+		
+		
+		
+		
+	</body>
+</htlm>
